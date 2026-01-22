bb2da811c0b991f4bbb8a5e121605ba20212a57d
chenzhg33 <chenzhg33@gmail.com>
2026-01-22 17:13:38 +0800

    add karabiner file 3

--- setup.sh
+++ setup.sh
@@ -1,23 +1,22 @@
-#! /bin/bash 
+#! /bin/bash
 list=(bash cheat other tmux vim zsh)
 
 mac_init() {
-    while IFS='' read -r line || [[ -n "$line" ]]; do
-        brew install "$line"
-    done < "./brew.txt"
+	while IFS='' read -r line || [[ -n "$line" ]]; do
+		brew install "$line"
+	done <"./brew.txt"
 
-    while IFS='' read -r line || [[ -n "$line" ]]; do
-        brew install --cask "$line"
-    done < "./cask.txt"
+	while IFS='' read -r line || [[ -n "$line" ]]; do
+		brew install --cask "$line"
+	done <"./cask.txt"
 
-    list+=(karabiner)
+	list+=(karabiner)
 }
 
 if [[ $(uname) == "Darwin" ]]; then
-    mac_init
+	mac_init
 fi
 
-
 for i in ${list[*]}; do
-    stow -t $HOME $i || exit -1
+	stow -t $HOME $i || exit -1
 done
